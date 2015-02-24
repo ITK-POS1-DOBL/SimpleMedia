d2f31af977ce9819168206b15e108ae2670d016e
Intruder <intruder1912@gmail.com>
2015-02-24 22:49:19 +0100

    added a hashmap in addition to the array list for some experimental purposes... works nicely for arraylist and hashmap media stores!

--- src/main/java/org/doblander/simplemedia/domain/MediaLibrary.java
+++ src/main/java/org/doblander/simplemedia/domain/MediaLibrary.java
@@ -6,6 +6,7 @@
 package org.doblander.simplemedia.domain;
 
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.logging.Level;
 import javax.enterprise.context.ApplicationScoped;
 import javax.inject.Named;
@@ -20,12 +21,18 @@ import java.util.logging.Logger;
 public class MediaLibrary {
 
     private static ArrayList<Medium> mediaList = new ArrayList();
+    
+    private static HashMap mediaStore = new HashMap();
 
     private static final Logger logger
             = Logger.getLogger("org.doblander.simplemedia.domain.MediaLibrary");
 
     public void insertMedium(String type, String title, String description) {
-        mediaList.add(new Medium(type, title, description));
+        Medium tempMedium = new Medium(type, title, description);
+        
+        mediaList.add(tempMedium);
+        mediaStore.put(tempMedium.getId(), tempMedium);
+        
         // Log-Output for debugging...
         logger.log(Level.INFO, "added medium: " + type + ", " + title
                 + ", " + description);
