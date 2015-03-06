7fc424bec40420a906dc49a971c416214c3d0961
Intruder <intruder1912@gmail.com>
2015-03-06 22:05:04 +0100

    refactored out some unnecessary code... now it looks a little more neat...

--- src/main/java/org/doblander/simplemedia/beans/InventarBean.java
+++ src/main/java/org/doblander/simplemedia/beans/InventarBean.java
@@ -21,16 +21,9 @@ import org.doblander.simplemedia.domain.MediumDTO;
 @RequestScoped
 public class InventarBean {
 
-    private String mediumIdString;
-    private String mediumTypeString;
-    private String mediumTitleString;
-    private String mediumDescString;
-    
     private boolean showResult = false;
     
     private List<MediumDTO> mediaList;
-
-    private MediumDTO[] dataTableEntries;
 
     @Inject
     private MediaLibrary mediaLib;
@@ -46,46 +39,12 @@ public class InventarBean {
 
     public void retrieveInventory() {
         this.setMediaList(mediaLib.getFullInventory());
-        this.dataTableEntries = new MediumDTO[mediaList.size()];
-        this.dataTableEntries = mediaList.toArray(new MediumDTO[1]);
         this.setShowResult(true);
     }
     
     public void resetResults() {
         setShowResult(false);
-        dataTableEntries = null;
-    }
-
-    public String getMediumIdString() {
-        return mediumIdString;
-    }
-
-    public void setMediumIdString(String mediumIdString) {
-        this.mediumIdString = mediumIdString;
-    }
-
-    public String getMediumTypeString() {
-        return mediumTypeString;
-    }
-
-    public void setMediumTypeString(String mediumTypeString) {
-        this.mediumTypeString = mediumTypeString;
-    }
-
-    public String getMediumTitleString() {
-        return mediumTitleString;
-    }
-
-    public void setMediumTitleString(String mediumTitleString) {
-        this.mediumTitleString = mediumTitleString;
-    }
-
-    public String getMediumDescString() {
-        return mediumDescString;
-    }
-
-    public void setMediumDescString(String mediumDescString) {
-        this.mediumDescString = mediumDescString;
+        mediaList = null;
     }
 
     public boolean isShowResult() {
@@ -94,14 +53,6 @@ public class InventarBean {
 
     public void setShowResult(boolean showResult) {
         this.showResult = showResult;
-    }
-
-    public MediumDTO[] getDataTableEntries() {
-        return dataTableEntries;
-    }
-
-    public void setDataTableEntries(MediumDTO[] dataTableEntries) {
-        this.dataTableEntries = dataTableEntries;
     }
 
     public List<MediumDTO> getMediaList() {
