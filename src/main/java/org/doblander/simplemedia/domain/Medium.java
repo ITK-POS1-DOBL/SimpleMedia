2d39a8ab7fe175d4acc0d6967dbd48dd980fa93f
Intruder <intruder1912@gmail.com>
2015-03-25 15:48:18 +0100

    PU not initialized correctly...

--- src/main/java/org/doblander/simplemedia/domain/Medium.java
+++ src/main/java/org/doblander/simplemedia/domain/Medium.java
@@ -5,6 +5,7 @@
  */
 package org.doblander.simplemedia.domain;
 
+import java.io.Serializable;
 import javax.persistence.Entity;
 import javax.persistence.Id;
 
@@ -15,7 +16,7 @@ import javax.persistence.Id;
  * @author intruder
  */
 @Entity
-public class Medium {
+public class Medium implements Serializable {
 
     @Id
     private long Id;
